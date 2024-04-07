bb81d00beb50c5a5c6829ee7958da0e5e03667e2
lunguradu-creator <149625218+lunguradu-creator@users.noreply.github.com>
2024-04-07 15:46:21 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+﻿document.getElementById("generateButton").addEventListener("click", function () {
+    var numberInput = parseInt(document.getElementById("numberInput").value);
+    if (isNaN(numberInput)) {
+        alert("Please enter a valid number.");
+        return;
+    }
+
+    var buttonsContainer = document.getElementById("buttonsContainer");
+    buttonsContainer.innerHTML = ""; // Clear previous buttons
+
+    var winningIndex = Math.floor(Math.random() * numberInput);
+
+    for (var i = 0; i < numberInput; ++i) {
+        var button = document.createElement("button");
+        button.textContent = "Feeling lucky? " + (i + 1);
+        button.classList.add("btn", "btn-danger"); // Add Bootstrap classes
+        button.type = "button"; // Set button type
+        button.addEventListener("click", function () {
+            if (this === buttonsContainer.children[winningIndex]) {
+                alert("WINNER - CONGRATULATIONS!");
+            } else {
+                alert("TRY AGAIN!");
+            }
+        });
+        buttonsContainer.appendChild(button);
+    }
+});
